c57e727e2ace299238dc228457d5f699318c62ae
Engineers <engineers@208-engineering-teacher.local>
2019-04-03 10:47:03 -0700

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Engineers on 4/3/19.
+//  Copyright © 2019 Engineers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
